b389f1e3b0de3fa6ca91d572236472081d7bc5ae
isthar <istharcahummarruffo@gmail.com>
2025-06-07 12:39:52 -0600

    titulo

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ const express = require('express');
 const app = express ();
 const path = require('path');
 const puerto = process.env.PORT || 3000;
-
+const mongoose = require('mongoose');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(__dirname + '/public'));
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongoDB conectado');
+}).catch(err => {
+  console.error('Error conectando a MongoDB:', err);
+});
+
 
 app.get('/', (req, res) => {
   res.render('index'); 
